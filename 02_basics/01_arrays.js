ef23a62aa2312d5fda372455f250644d8366a9d1
ANSH DEEP <ad0811@srmist.edu.in>
2025-01-26 12:22:36 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myarr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naagraj"]
+
+const myarr2 = new Array(1,2,3,4,5)
+
+// console.log(myarr[0])
+
+//Array Methods
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9))
+// console.log(myarr.indexOf(3))
+
+// const newArr = myarr.join()
+
+// console.log(myarr)
+// console.log(newArr)
+
+// slice or splice
+
+console.log("A ",myarr)
+
+const myn1 = myarr.slice(1,3)
+
+console.log(myn1)
+console.log("B ",myarr)
+
+const myn2 = myarr.splice(1,3)
+console.log("C ",myarr)
+console.log(myn2)
