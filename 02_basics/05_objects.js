d03fa997305f5dd2ba0d7b8921af0a66cf203f47
ANSH DEEP <ad0811@srmist.edu.in>
2025-02-07 14:38:26 +0000

    object de structure and json-api intro

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,32 @@
+const course = {
+    courseName: "js",
+    price: "999",
+    courseInstructor: "Ansh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course //destructuring
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "ansh")
+
+
+// {
+//     "name": "ansh",
+//     "courseName": "js",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
